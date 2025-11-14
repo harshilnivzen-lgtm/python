9b57125912f3e3d77a04835f6dab8b361321be99
harshilnivzen-lgtm <harshi.nivzen@gmail.com>
2025-11-14 18:32:24 +0530

    exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,18 +1,18 @@
-# try:
-#     age = int(input("Age: "))
-# except ValueError:
-#     print("You didn't enter a valid age.")
-# else:
-#     print("No exceptions were thrown.")
-# print("Execution continues.")
+try:
+    age = int(input("Age: "))
+except ValueError:
+    print("You didn't enter a valid age.")
+else:
+    print("No exceptions were thrown.")
+print("Execution continues.")
 
-# try:
-#   file: open("exceptions.py")
-#     age = int(input("Age: "))
-#     xfactor = 10 / age
-# except (ValueError, ZeroDivisionError):
-#     print("You didn't enter a valid age.")
-# else:
-#     print("No exceptions were thrown.")
-# finally:
-#     file.close()
+try:
+    file: open("exceptions.py")
+    age = int(input("Age: "))
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age.")
+else:
+    print("No exceptions were thrown.")
+finally:
+    file.close()
